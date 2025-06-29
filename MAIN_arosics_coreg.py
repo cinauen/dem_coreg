3da1fec233a6696b7feebd1889bc377f03eb0db9
Cornelia Inauen <cinauen@linux2.AWI-Potsdam.DE>
2025-06-29 20:57:01 +0200

    MAIN_arosics_coref refactor and commenting

--- MAIN_arosics_coreg.py
+++ MAIN_arosics_coreg.py
@@ -1,13 +1,30 @@
 
 '''
-Coregister a DEM to a reference DEM (using xdem)
+Coregister UAV data in horiyontal plane using agisoft using just RGB or
+combind with DEM
 
-inputs are raster files (.tif)
+inputs are:
+ - preprocessed raster files (.tif), reference and target (need to have same
+   coordinate system and extent)
+ - nodata mask (if defined to be used)
+parameters:
+ - all required parameters must be defined as a class in param_coreg.py
 
 Steps:
-run horizontal coregistration
+run horizontal coregistration using arosics
 
-nohup python ./MAIN_arosics_coreg.py &>./arosics_coreg.log &
+------
+usage: MAIN_arosics_coreg.py [-h] [--PARAM_CLASS PARAM_CLASS]
+
+Do horizontal co-registration based on mosaic
+options:
+  -h, --help            show this help message and exit
+  --PARAM_CLASS PARAM_CLASS
+                        name of class with parameters
+                        (default: UAV_coreg_steps_no_merge)
+
+or run in background with
+nohup python ./MAIN_arosics_coreg.py --PARAM_CLASS UAV_coreg_steps_no_merge &>./arosics_coreg.log &
 '''
 
 import sys
@@ -18,58 +35,45 @@ import pandas as pd
 import logging
 import datetime as dt
 
-import param_coreg
-import utils_dem_coreg
-import utils_arosics_coreg
-
 # get path to current directory to load modules
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 
-import control_file_templ
+import param_coreg
+import utils_preproc
+import utils_arosics_coreg
+
+import control_file
 
 
 def main(PARAM_CLASS="UAV_coreg_steps_no_merge"):
-    PATH = control_file_templ.get_proc_paths()
+    # %% ======= setup PATHS and parameters
+    PATH = control_file.get_proc_paths()
 
     # ------------ get param -----------
-    PARAM = utils_dem_coreg.create_param_class_instance(
+    PARAM = utils_preproc.create_param_class_instance(
         "param_coreg", PARAM_CLASS, PATH.PATH_BASE, 2)
 
     # -------------- setup time control ----------------
-    prof = utils_dem_coreg.setup_time_control()
+    prof = utils_preproc.setup_time_control()
 
-    # === create log file and initialize errors to be written to console
-    utils_dem_coreg.init_logging(
+    # --- create log file and initialize errors to be written to console
+    utils_preproc.init_logging(
         log_file=os.path.join(PARAM.PATH_IO,
         f'{PARAM.PROJ_NAME}_arosics_coreg_log_errors.log'),
         log_level=logging.DEBUG)
-    logging.info(
-        '==== Proc start ====:'
-        + dt.datetime.now().strftime('%Y-%m-%d_%H:%M'))
+    logging.info(f'=== Proc start ===: {utils_preproc.get_timenow_str()}')
 
-    # ====== read AOI (used for clipping)
-    AOI_FILE = os.path.join(
-        PARAM.PATH_IO, PARAM.PROCESSING_AOI)
-    AOI_coords, AOI_poly = utils_dem_coreg.read_transl_geojson_AOI_coords_single(
-        AOI_FILE, PARAM.EPSG_OUT)
-
-    # ===== get names of preprocessed files
+    # ----- get names of preprocessed files and mask
     df_merged = pd.read_csv(PARAM.preproc_files_df, sep='\t',
                             header=0).set_index('img_key')
 
-    mask_inv_save_path = PARAM.mask_file
+    if PARAM.use_mask_file:
+        mask_inv_save_path = PARAM.mask_file
+    else:
+        mask_inv_save_path = None
 
-    # do pre-coregistration in horizontal direction based on RGB image
-    # do global coregistration
-
-    # uses first file in target list to get correction parameters
-    # then appplies forrection to all files in target list
-
-    logging.info('==== Start global coreg: '
-                + dt.datetime.now().strftime('%Y-%m-%d_%H%M'))
-
-
+    # ---- define output paths for coregistration
     df_merged['f_path_global_coreg'] = df_merged.apply(
         lambda x: f'{x['f_path'].split('.')[0]}_coreg_global.tif'
         if 'REF' not in x['f_path'] else x['f_path'], axis=1)
@@ -78,11 +82,17 @@ def main(PARAM_CLASS="UAV_coreg_steps_no_merge"):
         lambda x: f'{x['f_path'].split('.')[0]}_coreg_local.tif'
         if 'REF' not in x['f_path'] else x['f_path'], axis=1)
 
+
+    # %% ====== global coregistration
+    # First file in target list is used to get correction parameters.
+    # The corrections are then then applied to all remaning files in
+    # target list
+    logging.info(f'==== Start global coreg: {utils_preproc.get_timenow_str()}')
     utils_arosics_coreg.do_global_coreg(
         df_merged.loc[PARAM.coreg_ref, 'f_path'],
         df_merged.loc[PARAM.coreg_target, 'f_path'].tolist(),
         df_merged.loc[PARAM.coreg_target, 'f_path_global_coreg'].tolist(),
-        coreg_mask=None,#mask_inv_save_path,
+        coreg_mask=mask_inv_save_path,
         CPU_num=PARAM.COREG_CPU_NUM,
         max_shift=PARAM.COREG_GLOBAL_MAX_SHIFT,
         window_size=(PARAM.COREG_GLOBAL_WINDOW_SIZE,
@@ -91,15 +101,22 @@ def main(PARAM_CLASS="UAV_coreg_steps_no_merge"):
         nodata=(np.nan, np.nan),  # nodata for target and reference
         resampling=PARAM.RESAMPLING_TYPE)
 
+    # save as .cog for faster display and r-save as .tif (since direct
+    # output of arosics required header file)
+    logging.info(f'==== Save global coreg: {utils_preproc.get_timenow_str()}')
     for i in df_merged.loc[PARAM.coreg_target, 'f_path_global_coreg'].tolist():
-        utils_dem_coreg.resave_tif_to_cog(i, chunks=PARAM.dask_chunks)
+        utils_preproc.resave_tif_to_cog(i, chunks=PARAM.dask_chunks)
 
-
+    # %% ====== local coregistration
+    # First file in target list is used to get correction parameters.
+    # The corrections are then then applied to all remaning files in
+    # target list
+    logging.info(f'==== Start local coreg: {utils_preproc.get_timenow_str()}')
     utils_arosics_coreg.apply_local_coreg(
         df_merged.loc[PARAM.coreg_ref, 'f_path'],
         df_merged.loc[PARAM.coreg_target, 'f_path_global_coreg'].tolist(),
         df_merged.loc[PARAM.coreg_target, 'f_path_local_coreg'].tolist(),
-        coreg_mask=None,#mask_inv_save_path,
+        coreg_mask=mask_inv_save_path,
         nodata=(np.nan, np.nan),  # nodata for target and reference
         grid_res_pix=PARAM.GRID_RES_PIX_LOCAL,
         max_shift=PARAM.COREG_LOCAL_MAX_SHIFT,
@@ -109,17 +126,18 @@ def main(PARAM_CLASS="UAV_coreg_steps_no_merge"):
         add_coreg_param_dict=PARAM.add_coreg_param_dict,
         resampling=PARAM.RESAMPLING_TYPE
         )
+    logging.info(f'==== Save local coreg: {utils_preproc.get_timenow_str()}')
     for i in df_merged.loc[PARAM.coreg_target, 'f_path_local_coreg'].tolist():
-        utils_dem_coreg.resave_tif_to_cog(i)
+        utils_preproc.resave_tif_to_cog(i)
 
+    # ====== save ouput file information and time statistics
     df_merged.to_csv(
         os.path.join(PARAM.PATH_IO, f'{PARAM.PROJ_NAME}_arosics_coreg_files.txt'),
         sep='\t', header=True)
-
-    utils_dem_coreg.save_time_stats(
+    utils_preproc.save_time_stats(
         prof, PARAM.PATH_IO, f'{PARAM.PROJ_NAME}_arosics_coreg')
 
-    print('----- FINISH ----')
+    logging.info(f'==== finished ====: {utils_preproc.get_timenow_str()}')
     return
 
 
@@ -128,7 +146,9 @@ if __name__ == '__main__':
         description='Do horizontal co-registration based on mosaic')
     parser.add_argument(
         '--PARAM_CLASS', type=str,
-        help=('name of Parameter class'), default="UAV_coreg_steps_no_merge")
+        help=('name of class with parameters (default: UAV_coreg_steps_no_merge)'),
+        default="UAV_coreg_steps_no_merge")
     args = parser.parse_args()
 
     main(**vars(args))
+
